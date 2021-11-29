983d02402bf71320d029ccb57d0512faada66b68
Hepburn <91041472+Hepburn23@users.noreply.github.com>
2021-11-29 03:16:15 -0600

    probando

---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_iii_p3_proyecto3/detallePedidoActivity.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.pdm_2021_iii_p3_proyecto3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class detallePedidoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_pedido)
+    }
+}
---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_iii_p3_proyecto3/localidadActivity.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.pdm_2021_iii_p3_proyecto3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class localidadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_localidad)
+    }
+}
